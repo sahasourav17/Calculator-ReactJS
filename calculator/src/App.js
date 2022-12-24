e45421d96f7b0b63b22d7d69fe8205ef37485b49
Sourav <sourav@pop-os.localdomain>
2022-12-24 15:19:30 +0600

    add functionality of digit and AC button

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,36 +1,68 @@
 import React, { useReducer } from "react";
 import "./styles.css";
+import DigitBtn from "./DigitBtn";
+import OperationBtn from "./OperationBtn";
 
-
-function reducer(state, action){
+export const ACTIONS = {
+    ADD_DIGIT : 'add-digit',
+    CHOOSE_OPERATION : 'choose-operation',
+    CLEAR :'clear',
+    DELETE_DIGIT : 'delete-digit',
+    EVALUATE : 'evaluate'
+}
+function reducer(state, {type,payload}){
+    switch(type) {
+        case ACTIONS.ADD_DIGIT:
+            if (payload.digit === "0" && state.curOperand === "0"){
+                return state
+            }
+            if (payload.digit === "." && state.curOperand.includes(".")){
+                return state
+            }
+            return {
+                ...state,
+                curOperand: `${state.curOperand || ""}${payload.digit}`
+            }
+        case ACTIONS.CLEAR:
+            return {}
+    }
 
 }
 
 function App(){
     const [{curOperand,prevOperand, operation }, dispatch] = useReducer(reducer,{})
+    // dispatch({
+    //     type :ACTIONS.ADD_DIGIT,
+    //     payload : {digit: 1}
+    // })
+
     return(
         <div className="calculator-grid">
             <div className="output">
                 <div className="prev-operand">{prevOperand} {operation}</div>
                 <div className="cur-operand">{curOperand}</div>
             </div>
-            <button className="span-two">AC</button>
+            <button className="span-two" onClick={()=>dispatch({
+                type : ACTIONS.CLEAR
+            })}>AC</button>
+
             <button>DEL</button>
-            <button>/</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>x</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>-</button>
-            <button>.</button>
-            <button>0</button>
+            <OperationBtn operation="/" dispatch={dispatch}/>
+            <DigitBtn digit="1" dispatch={dispatch}/>
+            <DigitBtn digit="2" dispatch={dispatch}/>
+            <DigitBtn digit="3" dispatch={dispatch}/>
+            <OperationBtn operation="x" dispatch={dispatch}/>
+            {/* <operationBtn /> */}
+            <DigitBtn digit="4" dispatch={dispatch}/>
+            <DigitBtn digit="5" dispatch={dispatch}/>
+            <DigitBtn digit="6" dispatch={dispatch}/>
+            <OperationBtn operation="+" dispatch={dispatch}/>
+            <DigitBtn digit="7" dispatch={dispatch}/>
+            <DigitBtn digit="8" dispatch={dispatch}/>
+            <DigitBtn digit="9" dispatch={dispatch}/>
+            <OperationBtn operation="-" dispatch={dispatch}/>
+            <DigitBtn digit="." dispatch={dispatch}/>
+            <DigitBtn digit="0" dispatch={dispatch}/>
             <button className="span-two">=</button>
         </div>
     )
---
+++ calculator/src/DigitBtn.js
@@ -0,0 +1,12 @@
+import { ACTIONS } from "./App";
+
+export default function DigitBtn({dispatch,digit}) {
+    return (
+        <button
+        onClick={()=> dispatch({
+            type : ACTIONS.ADD_DIGIT,
+            payload : {digit}
+        })}
+        > {digit}</button>
+    )
+}
