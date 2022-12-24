4162b7f6d7ea9354f6de73260e262c0d0eaed5a2
Sourav <sourav@pop-os.localdomain>
2022-12-24 16:04:47 +0600

    add functionality of all button

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -13,6 +13,14 @@ export const ACTIONS = {
 function reducer(state, {type,payload}){
     switch(type) {
         case ACTIONS.ADD_DIGIT:
+            if(state.overwrite){
+                return {
+                    ...state,
+                    curOperand: payload.digit,
+                    overwrite: false
+                }
+            }
+
             if (payload.digit === "0" && state.curOperand === "0"){
                 return state
             }
@@ -23,10 +31,119 @@ function reducer(state, {type,payload}){
                 ...state,
                 curOperand: `${state.curOperand || ""}${payload.digit}`
             }
+
+        case ACTIONS.CHOOSE_OPERATION:
+            if (state.curOperand == null && state.prevOperand == null){
+                return state
+            }
+            if (state.curOperand == null){
+                return {
+                    ...state,
+                    operation: payload.operation
+                }
+            }
+
+            if (state.prevOperand == null){
+                return{
+                    ...state,
+                    operation : payload.operation,
+                    prevOperand : state.curOperand,
+                    curOperand: null
+                }
+            }
+            
+            return {
+                ...state,
+                prevOperand: evaluate(state),
+                curOperand : null,
+                operation: payload.operation
+            }
+        
         case ACTIONS.CLEAR:
             return {}
+
+        case ACTIONS.DELETE_DIGIT:
+            if (state.overwrite){
+                return {
+                    ...state,
+                    overwrite: false,
+                    curOperand: null
+                }
+            }
+            else if(state.curOperand == null){
+                return state
+            }
+            else if(state.curOperand.length === 1){
+                return {
+                    ...state,
+                    curOperand: null
+                }
+            }
+            else{
+                return {
+                    ...state,
+                    curOperand: state.curOperand.slice(0,-1) //remove last digit
+                }
+            }
+        
+        case ACTIONS.EVALUATE:
+            if (state.operation == null ||
+                state.curOperand == null ||
+                state.prevOperand == null) {
+                    return state
+                }
+            
+            return {
+                ...state,
+                overwrite:true,
+                prevOperand : null,
+                operation: null,
+                curOperand: evaluate(state)
+            }
     }
 
+}
+
+function evaluate({curOperand,prevOperand,operation}) {
+    const curr = parseFloat(curOperand);
+    const prev = parseFloat(prevOperand);
+    if(isNaN(prev) || isNaN(curr)){
+        return ""
+    }
+
+    let result = "";
+    switch(operation){
+        case "+":
+            result = prev+curr
+            break
+        case "-":
+            result = prev-curr
+            break
+        case "x":
+            result = prev*curr
+            break
+        case "/":
+            if(curOperand !== '0'){
+                result = prev / curr
+                break
+            }
+            else{
+                result = "Error!!"
+            }
+    }
+    return result.toString()
+}
+
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us",{
+    maximumFractionDigits: 0
+})
+
+function formatOperand(operand){
+    if (operand == null) return
+    const[integer,decimal] = operand.split('.')
+    if (decimal == null) return INTEGER_FORMATTER.format(integer)
+
+    return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 
 function App(){
@@ -39,14 +156,16 @@ function App(){
     return(
         <div className="calculator-grid">
             <div className="output">
-                <div className="prev-operand">{prevOperand} {operation}</div>
-                <div className="cur-operand">{curOperand}</div>
+                <div className="prev-operand">{formatOperand(prevOperand)} {operation}</div>
+                <div className="cur-operand">{formatOperand(curOperand)}</div>
             </div>
             <button className="span-two" onClick={()=>dispatch({
                 type : ACTIONS.CLEAR
             })}>AC</button>
 
-            <button>DEL</button>
+            <button onClick={()=>dispatch({
+                type : ACTIONS.DELETE_DIGIT
+            })}>DEL</button>
             <OperationBtn operation="/" dispatch={dispatch}/>
             <DigitBtn digit="1" dispatch={dispatch}/>
             <DigitBtn digit="2" dispatch={dispatch}/>
@@ -63,7 +182,9 @@ function App(){
             <OperationBtn operation="-" dispatch={dispatch}/>
             <DigitBtn digit="." dispatch={dispatch}/>
             <DigitBtn digit="0" dispatch={dispatch}/>
-            <button className="span-two">=</button>
+            <button className="span-two" onClick={()=>dispatch({
+                type : ACTIONS.EVALUATE
+            })}>=</button>
         </div>
     )
 }
